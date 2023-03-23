f3322d4c8b29b3c7de01967ee420d550c5d6a19b
8311yua <tennnemu5473@docomo.ne.jp>
2023-03-23 16:14:45 +0900

    Update products.php

--- products.php
+++ products.php
@@ -30,12 +30,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
             <div class="container">
-<<<<<<< HEAD
-<<<<<<<< HEAD:products.php
                 <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.php">Shitamichi's Bakery</a>
-========
-                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">Shitamichi's Bakery</a>
->>>>>>>> a28e0b0f05129071a120b0f727bffcf710615f0a:products.html
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mx-auto">
@@ -43,16 +38,6 @@
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="list.php">Menu</a></li>
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.php">Reservation</a></li>
                         <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.php">Store</a></li>
-=======
-                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">Shitamichi's Bakery</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mx-auto">
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.html">Home</a></li>
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">Menu</a></li>
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">Reservation</a></li>
-                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">Store</a></li>
->>>>>>> a28e0b0f05129071a120b0f727bffcf710615f0a
                     </ul>
                 </div>
             </div>
