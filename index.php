87127cfb201483b2dae3a6fbf8a4c2b70b205fac
HarunaTaguchi <tagu0004@gmail.com>
2023-04-25 13:40:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
                     <div class="product-item-title d-flex">
                         <div class="bg-faded p-5 d-flex ms-auto rounded">
                             <h2 class="section-heading mb-0">
-                                <span class="section-heading-upper">ホットでもアイスでも♡</span>
+                                <span class="section-heading-upper">♡ホットでもアイスでも♡</span>
                                 <span class="section-heading-lower">drink</span>
                             </h2>
                         </div>
