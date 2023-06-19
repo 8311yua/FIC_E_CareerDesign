7bf3f2eda3080f807842929ea47c064cd6d7b685
8311yua <tennnemu5473@docomo.ne.jp>
2023-06-19 13:59:19 +0900

    test

--- menu_test.php
+++ menu_test.php
@@ -1,4 +1,6 @@
-<!-- testケース -->
+<!-- 現メニュー一覧 -->
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,102 +8,99 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <!-- ページタイトル　＆　ページアイコン -->
+        <!-- タイトル -->
         <title>Shitamichi's Bakery</title>
-        <link rel="icon" type="image/x-icon" href="assets/images/logo2.png" />
-        <!-- Font Awesome icons (free version)-->
+        <link rel="icon" type="image/x-icon" href="../assets/images/logo2.png" />
+        <!-- icons-->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- fonts-->
+        <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
         <!-- CSS-->
-        <link href="css/styles.css" rel="stylesheet">
-        <link href="css/test.css"   rel="stylesheet">
-    </head>
-<body>
-<!-- データベース -->
-<?php require 'server_connection.php' ?>
-<!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.php">Shitamichi's Bakery</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto">
-                
-                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="test.php#food">food</a></li>
-                <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="test.php#drink">drink</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<section class="product spad">
-    <div class="container">
-        <div class="col-xl-9 col-lg-10 mx-auto">
-            <div class="bg-faded rounded p-5">
-                <!-- food -->
-                <h2 id="food">Food</h2>
-                <div class="row">                    
-                    <?php
-                    foreach($pdo->query('select * from Food') as $row){
-                        $id = $row['id'];
-                        echo '<div class="col-lg-4 col-md-6 col-sm-6">';
-                        echo '<div class="product__item"><tr>';
-                        // ここで、ホップアップウィンドウで開かせる(画像) 
-                        echo '<label for="pu-on"><img src="','assets/images/',$row['image'],'"class="product__item__pic"></label>';
-                        echo '<div class="product__item__text">';
-                        // ここに上のコードを加える
-                        // echo '<h5><label for="pu-on"></label></h5>';
-                        echo '<h5><a href="test.php?id=',$id,'">',$row['name'],'</a></h5>';
-                        echo '<h5>','¥', $row['price'], '</h5>';
-                        echo '</div>';
-                        echo '</div>';
-                        echo '</div>';
-                    }
-                    ?>
-                </div>
-                <!-- drink -->
-                <!-- まだ手を加えてない -->
-                <h2 id="drink">Drink</h2>
-                <div class="row">
-                    <?php
-                    foreach($pdo->query('select * from drink') as $row){
-                        echo '<div class="col-lg-4 col-md-6 col-sm-6">';
-                        echo '<div class="product__item"><tr>';
-                        // ここで、ホップアップウィンドウで開かせる(画像) 
-                        echo '<label for="pu-on"><img src="','assets/images/',$row['image'],'"class="product__item__pic"></label>';
-                        echo '<div class="product__item__text">';
-                        // ここに上のコードを加える
-                        echo '<h5><a href="test.php?id=',$id,'">',$row['name'],'</a></h5>';
-                        echo '<h5>','¥', $row['prices'],'~¥',$row['pricel'],'</h5>';
-                        echo '</div>';
-                        echo '</div>';
-                        echo '</div>';
-                    }
-                    ?>
-                </div>
-                    
-                    <!-- echo'<input type="checkbox" id="pu-on">';
-                    echo'<div class="pu">';
-                    echo'<label for="pu-on" class="icon-close">×</label>';
-                    echo'<div class="pu-content">';
-                    echo '<a href="gameDetails.php?id=', $id,'"><div class="product__item__pic set-bg" data-setbg="','assets/images/',$row['image'],'">';
-                    echo'<label for="pu-on"><img src="','assets/images/',$row['image'],'"class="product__item__pic"></label>';
-                    echo'</div>';
-                    echo'</div>';
-                    echo'</div>';
-                    -->
+        <link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+        <!-- <link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-1/css/6-1-1.css"> -->
 
+        <link href="css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" href="css/mystyle.css">
+    </head>
+    <header>
+        <div class="wrapper">
+            <h1 class="heading site-heading text-center text-faded d-none d-lg-block">Shitamichi's Bakery</h1>
+                <!-- <span class="site-heading-upper text-primary mb-3">
+                    <img style="width: 10%;" src="assets/images/logo4.png" alt="..." />
+                </span> -->
+                <!-- <span class="site-heading-lower">Shitamichi's Bakery</span><br>
+                <span class="site-heading-lower" style="font-size: 50%;">思わず笑顔になるおいしいコーヒーを提供しています。<br>クッキーやクロワッサン、タルトやケーキなどのお菓子と一緒にいかがでしょうか。</span>
+                -->
+                <ul class="slider">
+                    <li class="slider-item slider-item01"></li>
+                    <li class="slider-item slider-item02"></li>
+                    <li class="slider-item slider-item03"></li>
+                    <li class="slider-item slider-item04"></li>
+                </ul>
+            
+        </div>
+    </header>
+        <!-- データベース -->
+        <?php require 'server.php' ?>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.php">Shitamichi's Bakery</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Menu_test#food">food</a></li>
+                        <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="Menu_test#drink">drink</a></li>
+                    </ul>
+                </div>
             </div>
-            <input type="checkbox" id="pu-on">
-                <div class="pu">
-                    <label for="pu-on" class="icon-close">×</label>
-                    <div class="pu-content">
-                        <h1>～～商品～～</h1>
-                        <label for="pu-on"><img src="','assets/images/',$row['image'],'"class="product__item__pic"></label>
+        </nav>
+        <section class="product spad">
+            <div class="container">
+                <div class="col-xl-9 col-lg-10 mx-auto">
+                    <div class="bg-faded rounded p-5">
+                        <!-- food -->
+                        <h2 id="food">Food</h2>
+                        <div class="row">                    
+                            <?php
+                            foreach($pdo->query('select * from food') as $row){
+                                $id = $row['id'];
+                                echo '<div class="col-lg-4 col-md-6 col-sm-6">';
+                                echo '<div class="product__item"><tr>';
+                                // echo '<img src="','assets/images/',$row['image'],'"class="product__item__pic">';
+                                echo '<div class="product__item__text">';
+                                echo '<h5><a href="Food.php?id=',$id,'">',$row['name'],'</a></h5>';
+                                echo '<h5>','¥', $row['price'], '</h5>';
+                                echo '</div>';
+                                echo '</div>';
+                                echo '</div>';
+                            }
+                            ?>
+                        </div>
+                        <!-- drink -->
+                        <h2 id="drink">Drink</h2>
+                        <div class="row">
+                            <?php
+                            foreach($pdo->query('select * from drink') as $row){
+                                $id = $row['id'];
+                                echo '<div class="col-lg-4 col-md-6 col-sm-6">';
+                                echo '<div class="product__item"><tr>';
+                                // echo '<img src="','assets/images/',$row['image'],'"class="product__item__pic">';
+                                echo '<div class="product__item__text">';
+                                echo '<h5><a href="Drink.php?id=',$id,'">',$row['name'],'</a></h5>';
+                                echo '<h5>','¥', $row['prices'],'~¥',$row['pricel'],'</h5>';
+                                echo '</div>';
+                                echo '</div>';
+                                echo '</div>';
+                            }
+                            ?>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</section>
+        </section>
+        <script src="js/myjs.js"></script>
+    </body>
+</html>
