34a83a7d8fe8873439d4446b8ba703b594f411c2
HarunaTaguchi <tagu0004@gmail.com>
2023-06-29 15:24:17 +0900

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$pdo =new PDO('mysql:host=localhost;dbname=pan;charset=utf8', 'staff', 'password');
+$pdo =new PDO('mysql:host=mysql215.phy.lolipop.lan;dbname=LAA1530773-cafe22teame;charset=utf8', 'LAA1530773', 'Fic20222023It');
 ?>
 <!-- データベース接続の際は下記のコードを入力すること -->
 <!-- require 'server.php' -->
