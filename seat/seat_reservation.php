068f9c91308c051bed363c9d07a75ea37b987daf
HarunaTaguchi <tagu0004@gmail.com>
2023-06-29 17:19:42 +0900

    Update seat_reservation.php

--- seat/seat_reservation.php
+++ seat/seat_reservation.php
@@ -1,10 +1,28 @@
-<!doctype html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <title>座席予約</title>
-  <link rel="stylesheet" href="seat.css">
-</head>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <!-- タイトル -->
+        <title>Shitamichi's Bakery</title>
+        <link rel="icon" type="image/x-icon" href="assets/images/logo2.png" />
+        <!-- icons-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+        <!-- CSS-->
+        <link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/reset.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+        <!-- <link rel="stylesheet" type="text/css" href="https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-1/css/6-1-1.css"> -->
+
+        <link href="../css/styles.css" rel="stylesheet" />
+        <link href="../css/mystyle.css"rel="stylesheet" type="text/css" >
+        <link href="seat.css" rel="stylesheet" />
+
+    </head>
 <body>
     <form class="form" action="seat_check.php" method="post">
         <img src="seat.png" alt="座席表">
