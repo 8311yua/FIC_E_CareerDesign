62cc506665b866f761d53664641e3416c677fadf
HarunaTaguchi <tagu0004@gmail.com>
2023-06-30 12:51:15 +0900

    Update seat_reservation.php

--- seat/seat_reservation.php
+++ seat/seat_reservation.php
@@ -32,10 +32,10 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mx-auto ul-nav">
-                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="index.html"><span>Home</span><br>ホーム</a></li>
-                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="menu.php"><span>Menu</span><br>商品予約</a></li>
-                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="seat/seat_reservation.php"><span>Reservation</span><br>座席予約</a></li>
-                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="login/login_input.php"><span>login</span><br>ログイン</a></li>
+                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="../index.html"><span>Home</span><br>ホーム</a></li>
+                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="../menu.php"><span>Menu</span><br>商品予約</a></li>
+                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="seat_reservation.php"><span>Reservation</span><br>座席予約</a></li>
+                        <li class="nav-item li-nav px-lg-4"><a class="nav-link text-uppercase" href="../login/login_input.php"><span>login</span><br>ログイン</a></li>
                     </ul>
                 </div>
             </div>
